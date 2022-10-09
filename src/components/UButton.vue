5d2763476e6102d324ed61368d7303bb639f8b59
Mykhailo Kohutka <115368431+illumi1717@users.noreply.github.com>
2022-10-10 00:07:49 +0300

    add buttons

---
+++ src/components/UButton.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="u-button">
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "u-button"
+    }
+</script>
--- src/views/MainView.vue
+++ src/views/MainView.vue
@@ -1,10 +1,20 @@
 <template>
     <div class="main-view">
+        <div class="menu">
+            <u-button>Start</u-button>
+            <u-button>Settings</u-button>
+        </div>
     </div>
 </template>
 
 <script>
+    import UButton from "@/components/UButton"
+
     export default {
-        name: 'main-view'        
+        name: 'main-view',
+        
+        components: {
+            UButton
+        }
     }
 </script>
